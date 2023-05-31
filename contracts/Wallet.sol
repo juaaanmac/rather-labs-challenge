82f26fa00cc10dcf70dd6dd1d48ba531901220b9
juaaanmac <juanmacri@outlook.com>
2023-05-31 14:40:25 -0300

    contructor for wallet contract

--- contracts/Wallet.sol
+++ contracts/Wallet.sol
@@ -1,5 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
 contract Wallet {
+    address private _router;
+    IERC20 private _token1;
+    IERC20 private _token2;
+
+    constructor(address router, address token1, address token2) {
+        _router = router;
+        _token1 = IERC20(token1);
+        _token2 = IERC20(token2);
+    }
 }
